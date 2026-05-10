52e6762b54f39225acd9276b29abb366fdc456d4
orkessantri <orkessantri@gmail.com>
2026-05-11 02:40:18 +0700

    Update song.js

--- js/song.js
+++ js/song.js
@@ -20,7 +20,7 @@ async function loadSong(){
     await res.json();
 
   const song =
-    songs.find(s => s.id == songId);
+    songs.find(s => s.id == id);
   
   console.log(songId);
 console.log(song);
