dbfcceb0f68f8f951644856d7df955bbae037593
orkessantri <orkessantri@gmail.com>
2026-05-17 01:25:07 +0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,3 +4,35 @@ const kategoriConfig = [
   { name:"DUNGDAT" },
   { name:"POPINDO" }
 ];
+
+function validateSongsCategories(songs){
+
+  const validCategories =
+
+    kategoriConfig.map(
+      k => k.name
+    );
+
+  songs.forEach(song => {
+
+    if(
+      !validCategories.includes(
+        song.category
+      )
+    ){
+
+      console.warn(
+
+        `
+UNKNOWN CATEGORY
+
+Song:
+${song.title}
+
+Category:
+${song.category}
+        `
+      );
+    }
+  });
+}
