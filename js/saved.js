dfddb741e84343634daad94e4ae13ba7dfc3fa3f
orkessantri <orkessantri@gmail.com>
2026-05-17 23:10:51 +0700

    Update saved.js

--- js/saved.js
+++ js/saved.js
@@ -78,6 +78,13 @@ function renderSaved(){
 
           <button
             class="saved-btn"
+            onclick="exportSetlist(${setlist.id})"
+          >
+            ↓
+          </button>
+
+          <button
+            class="saved-btn"
             onclick="deleteSetlist(${setlist.id})"
           >
             ✕
@@ -221,4 +228,150 @@ function duplicateSetlist(id){
   renderSaved();
 }
 
+function exportSetlist(id){
+
+  const selected =
+
+    savedSetlists.find(
+      s => s.id === id
+    );
+
+  if(!selected) return;
+
+  // CONVERT JSON
+  const data = JSON.stringify(
+    selected,
+    null,
+    2
+  );
+
+  // BLOB
+  const blob = new Blob(
+
+    [data],
+
+    {
+      type:"application/json"
+    }
+
+  );
+
+  // URL
+  const url =
+    URL.createObjectURL(blob);
+
+  // LINK
+  const link =
+    document.createElement('a');
+
+  link.href = url;
+
+  // FILE NAME
+  link.download =
+
+    selected.name
+      .replace(/\s+/g,'-')
+      .toLowerCase()
+
+    + ".bnb";
+
+  // DOWNLOAD
+  link.click();
+
+  // CLEAN
+  URL.revokeObjectURL(url);
+}
+
 renderSaved();
+
+document
+  .getElementById(
+    "importInput"
+  )
+  .addEventListener(
+
+    "change",
+
+    importSetlistFile
+
+);
+
+async function importSetlistFile(e){
+
+  const file =
+    e.target.files[0];
+
+  if(!file) return;
+
+  try{
+
+    // READ FILE
+    const text =
+      await file.text();
+
+    // PARSE JSON
+    const imported =
+      JSON.parse(text);
+
+    // VALIDASI
+    if(
+      !imported.name ||
+      !imported.songs
+    ){
+
+      alert(
+        "File invalid"
+      );
+
+      return;
+    }
+
+    // AMBIL STORAGE
+    savedSetlists =
+
+      JSON.parse(
+
+        localStorage.getItem(
+          "savedSetlists"
+        )
+
+      ) || [];
+
+    // NEW ID
+    imported.id =
+      Date.now();
+
+    // PUSH
+    savedSetlists.push(
+      imported
+    );
+
+    // SAVE
+    localStorage.setItem(
+
+      "savedSetlists",
+
+      JSON.stringify(
+        savedSetlists
+      )
+
+    );
+
+    // RENDER
+    renderSaved();
+
+    alert(
+      "Import berhasil 🔥"
+    );
+
+  }catch(err){
+
+    console.log(err);
+
+    alert(
+      "Import gagal"
+    );
+
+  }
+
+}
